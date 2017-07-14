42496e272350c61ca6cb660dc505e337ad88bf50
Lora Milcheva <lora.milcheva@outlook.com>
2017-07-14 10:52:11 +0300

    Fixed some initial problems. Changed structure a little bit. Added Enter key functionality. Added all needed functions as names (to ease merging).

--- ListProcessing.js
+++ ListProcessing.js
@@ -1,51 +1,82 @@
-let theArray = [];
-
 function process() {
+
+    let theArray = [];
+
+    let input = $('#command');
+    let result = $('#result');
+    let executeBtn = $('#executeBtn');
+
+    executeBtn.on('click', execute);
+    input.keypress(function(event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') {
+            execute();
+        }
+    });
+
     let commands = {
         append: function ([a]) {
             if (!a) {
                 throw new Error("invalid")
             }
-            // console.log(a);
-
             theArray.push(a);
+        },
+        prepend: function ([a]) {
+
         },
         reverse: function () {
             theArray = theArray.reverse();
         },
         insert: function ([a, b]) {
-
-            if(a > theArray.length) {
-                throw new Error("new error");
+            if ((a > theArray.length - 1) || (a < 0)) {
+                throw new Error(`Error: invalid index ${a}`);
             }
             theArray.splice(+a, 0, b);
+        },
+        delete: function ([a]) {
 
+        },
+        rollLeft: function () {
+
+        },
+        rollRight: function () {
 
         },
         sort: function () {
             theArray.sort() + '\n';
+        },
+        count: function ([a]) {
+
+        },
+        end: function () {
 
         }
-
     };
-    let input = $('#command');
-    let result = $('#result');
 
+    function execute() {
 
-    } else {
-        if (input.val()) {
+        let tokens = input.val().split(' ').filter(w => w !== '');
+        let currentCommand = tokens[0];
+        let data = tokens.slice(1);
+
+        if (currentCommand) {
             try {
-                let split = input.val().split(" ").filter(w => w !== '');
-                commands[split[0]](split.slice(1)); //roll right
-                // console.log(input.val());
-                result.text(result.text() + theArray.join(" ") + '\n');
+                if (theArray.length === 0) {
+                    input.val().split(' ').filter(w => w !== '').forEach(e => theArray.push(e));
+                    result.text(result.text() + theArray.join(' ') + '\n');
+                } else {
+                    if (!commands.hasOwnProperty(currentCommand)) {
+                        throw new Error('Error: invalid command');
+                    }
+                    commands[currentCommand](data);
+                    result.text(result.text() + theArray.join(' ') + '\n');
+                }
             } catch (err) {
                 result.text(result.text() + err.message + '\n');
                 window.alert(err.message)
+            } finally {
+                input.val('');
             }
         }
-
     }
-
-    input.val('');
 }
